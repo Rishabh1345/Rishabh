24d75d4fa8a2ac42bcbb3b0d9f2cb77416415332
Rishabh Shrivas <125146524+Rishabh1345@users.noreply.github.com>
2023-02-22 22:46:09 +0530

    Add files via upload

---
+++ Demo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	a = 12;
+	b = 2;
+	
+	printf("a + b = %d\n", a+b );
+	printf("a - b = %d\n", a-b );
+	printf("a * b = %d\n", a*b );
+	printf("a / b = %d\n", a/b );
+	printf("a % b = %d\n", a%b );
+
+	return 0;
+}
---
+++ Demo.py
@@ -0,0 +1,5 @@
+Num = int (input("Enter a Number;"))
+if (num ℅ 2) == 0;
+  print ("{0} is even", format (num))
+else;
+  print ("{0} is odd", format (num))
